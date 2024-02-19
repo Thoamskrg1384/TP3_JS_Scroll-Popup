cc0aee2efc1afd3bcbaa2f3bb4fd77538a7210de
Thoams Krg <thoams.krieg@gmail.com>
2024-02-19 17:04:47 +0100

    Fin de TP ok

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-// Créer un événement au scroll
+window.addEventListener("scroll", () => {
+  // Navbar effect
+  if (window.scrollY > 20) {
+    navbar.style.height = "45px";
+  } else {
+    navbar.style.height = "90px";
+  }
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+  // Image
+  if (scrollValue > 0.45) {
+    imgImprovise.style.opacity = 1;
+    imgImprovise.style.transform = "none";
+  }
+
+  // Popup
+  if (scrollValue > 0.8) {
+    popup.style.opacity = 1;
+    popup.style.transform = "none";
+  }
+});
+
+// Fermeture définitive du popup
+closeBtn.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+// Réduire la navbar quand on descend vers le site, la remettre à sa taille initiale si on remonte tout en haut
 
 // Faire apparaitre l'image de la partie improvise
 
